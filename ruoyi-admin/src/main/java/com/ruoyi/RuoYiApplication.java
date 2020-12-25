50bbebbfe938c2d535ce26bcf8ba681b8340d886
adonis <a1121732664@outlook.com>
2020-12-25 22:31:29 +0800

    feature/vueLearn 对整体结构进行初始化并确立目标

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -3,33 +3,15 @@ package com.ruoyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.util.HtmlUtils;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
-        //TODO 目标爬取微信读书部分书籍名称
-        //TODO 实现改良的雪花算法及自动赋值createTime及updateTime
-        //TODO 2020-12-26 完成vue的组件化学习
-        //TODO 支持聚合支付的代码 2021-1
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
     }
 }
